cca3606dea1df46e45e4b6b51977e4b8e55879fe
Coud <nikielprzemek@gmail.com>
2017-05-28 00:19:53 +0200

    about info added

--- InfoSejm/app/src/main/java/org/infosejm/infosejm/Menu.java
+++ InfoSejm/app/src/main/java/org/infosejm/infosejm/Menu.java
@@ -1,6 +1,7 @@
 package org.infosejm.infosejm;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -29,6 +30,9 @@ public class Menu extends AppCompatActivity {
     }
 
     public void about(View view) {
-
+        AlertDialog alertDialog = new AlertDialog.Builder(Menu.this).create();
+        alertDialog.setTitle("O aplikacji");
+        alertDialog.setMessage("Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. Phasellus fermentum in, dolor.");
+        alertDialog.show();
     }
 }
