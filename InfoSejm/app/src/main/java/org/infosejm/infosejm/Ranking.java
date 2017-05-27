4f016072dbcb2d0e2a9f4df766a95d9508d86dd9
Coud <nikielprzemek@gmail.com>
2017-05-27 19:17:05 +0200

    ranking activity with 10 people added

--- InfoSejm/app/src/main/java/org/infosejm/infosejm/Ranking.java
+++ InfoSejm/app/src/main/java/org/infosejm/infosejm/Ranking.java
@@ -1,23 +1,33 @@
 package org.infosejm.infosejm;
 
+import android.support.annotation.MainThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.Arrays;
 import java.util.Comparator;
 
 public class Ranking extends AppCompatActivity {
+    TextView ranking;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
+        ranking = (TextView) findViewById(R.id.ranking);
+        rankingList();
+    }
 
-        Log.i("wiadomosc", MainActivity.members[459][4]);
-        Log.i("wiadomosc", MainActivity.members[459][0]);
-        Log.i("wiadomosc", MainActivity.members[459][1]);
-        Log.i("wiadomosc", MainActivity.members[459][2]);
-        Log.i("wiadomosc", MainActivity.members[459][3]);
-
+    public void rankingList() {
+        StringBuilder text = new StringBuilder();
+        int position = 1;
+        for (int i = 459; i > 449; i--) {
+            double d = Double.parseDouble(MainActivity.members[i][3]);
+            double wydatki = Math.round(d * 100.0) / 100.0;
+            text.append(position).append(". ").append(MainActivity.members[i][4]).append(" Partia: ").append(MainActivity.members[i][2]).append(" Wydatki: ").append(wydatki).append("\n");
+            position++;
+        }
+        ranking.setText(text);
     }
 }
